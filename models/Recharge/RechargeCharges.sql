9bd27f7eeaf918c475d9031409d152712a3c739e
SarasAnalytics-Daton <69815945+saras-daton@users.noreply.github.com>
2023-05-25 18:26:13 +0530

    Update RechargeCharges.sql

--- models/Recharge/RechargeCharges.sql
+++ models/Recharge/RechargeCharges.sql
@@ -24,7 +24,7 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
 {% endif %}
 
 {% set table_name_query %}
-{{set_table_name('%recharge_bq_charges')}}    
+{{set_table_name('%recharge%charges')}}    
 {% endset %}  
 
 {% set results = run_query(table_name_query) %}
