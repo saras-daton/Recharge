8b9aa0b55b8a02525e1ada5257f2bb00a6ab04af
SarasAnalytics-Daton <69815945+saras-daton@users.noreply.github.com>
2023-05-25 18:28:38 +0530

    Update RechargeSubscriptions.sql

--- models/Recharge/RechargeSubscriptions.sql
+++ models/Recharge/RechargeSubscriptions.sql
@@ -19,7 +19,7 @@
     {% endif %}
 
     {% set table_name_query %}
-        {{set_table_name('%recharge_bq_subscriptions')}}    
+        {{set_table_name('%recharge%subscriptions')}}    
     {% endset %} 
 
     {% set results = run_query(table_name_query) %}
