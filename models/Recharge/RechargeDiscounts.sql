7ccc8a0729069ce716479d76c9ba864093dd7fbd
SarasAnalytics-Daton <69815945+saras-daton@users.noreply.github.com>
2023-05-25 18:27:04 +0530

    Update RechargeDiscounts.sql

--- models/Recharge/RechargeDiscounts.sql
+++ models/Recharge/RechargeDiscounts.sql
@@ -20,7 +20,7 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
 {% endif %}
 
 {% set table_name_query %}
-{{set_table_name('%recharge_earthbreeze_discounts')}}    
+{{set_table_name('%recharge%discounts')}}    
 {% endset %}  
 
 {% set results = run_query(table_name_query) %}
